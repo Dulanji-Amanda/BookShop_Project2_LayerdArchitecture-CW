ca6b5b2c09dc53f181a83ecbe0c23c12d88fa0a8
Dulanji <amandasathsarani03@gmail.com>
2025-02-11 08:26:41 -0800

    update

--- src/main/java/org/example/stockverse/db/DBConnection.java
+++ src/main/java/org/example/stockverse/db/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
 
     private DBConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshop", "root", "Kavindu@1125");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshop", "root", "Ijse@1234");
     }
 
     public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
